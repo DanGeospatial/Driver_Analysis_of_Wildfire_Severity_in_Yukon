a3219f673c68fd06cf4905ff5835418349df0e56
DanGeospatial <daniel.nelson.ca@gmail.com>
2024-07-29 22:58:56 -0400

    ray method done without feature selection

--- Train.py
+++ Train.py
@@ -2,3 +2,80 @@
 Train wildfire regression model using XGBoost
 Optimize with Ray Tune and HyperOPT
 """
+
+import ray
+from ray import tune
+from ray.tune.experiment import trial
+from ray.tune.search.hyperopt import HyperOptSearch
+from hyperopt import hp
+from ray.data import Dataset, Preprocessor
+from ray.train.xgboost import XGBoostTrainer
+import xgboost
+
+# Import Data
+dNBR3Y = "D:/Research/Masters/8_RStudio/Input Data/ERA5/total/dNBR_total_3y.csv"
+
+ray.init(include_dashboard=False, runtime_env={"working_dir": "E:/Temp"})
+
+# Preprocess Data
+dataset = ray.data.read_csv(dNBR3Y)
+train, valid = dataset.train_test_split(test_size=0.25)
+
+
+def objective(config):
+
+    parameters = {
+        "eval_metric": "rmse",
+        "eta": config["eta"],
+        "gamma": config["gamma"],
+        "max_depth": config["max_depth"],
+        "min_child_weight": config["min_child_weight"],
+        "subsample": config["subsample"],
+        "colsample_bytree": config["colsample_bytree"],
+        "lambda": config["lambda"],
+        "alpha": config["alpha"]
+    }
+
+    trainer = XGBoostTrainer(
+        datasets={"train": train, "valid": valid},
+        label_column="dNBR",
+        params=parameters
+    )
+
+    trained = trainer.fit()
+    test_rmse = trained.metrics.get("rmse")
+
+    return {"rmse": test_rmse}
+
+
+method = HyperOptSearch()
+samples = 200
+
+search_config = {
+    "eta": tune.loguniform(0.01,1),
+    "gamma": tune.randint(0,10),
+    "max_depth": tune.randint(3,10),
+    "min_child_weight": tune.randint(1,5),
+    "subsample": tune.loguniform(0.5,1),
+    "colsample_bytree": tune.loguniform(0.5,1),
+    "lambda": tune.randint(1,5),
+    "alpha": tune.randint(0,5)
+}
+
+
+tuner = tune.Tuner(
+    objective,
+    tune_config=tune.TuneConfig(
+        metric="rmse",
+        mode="min",
+        search_alg=method,
+        num_samples=samples,
+        trial_dirname_creator=trial.TRIAL_ID
+    ),
+    param_space=search_config,
+)
+
+results = tuner.fit()
+print("Optimal hyperparameters: ", results.get_best_result().config)
+print(results.get_best_result())
+

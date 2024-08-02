dce9cb369dfbb6cb60e9947bfa608fdc6c0d5a20
DanGeospatial <daniel.nelson.ca@gmail.com>
2024-08-02 14:11:54 -0400

    begin creating Compiler.py

--- Data_Processing_and_Conversion/Compiler.py
+++ Data_Processing_and_Conversion/Compiler.py
@@ -1,4 +1,7 @@
 """
 This will control creating the large wildfire dataset
 """
+import xarray as xr
+import rasterio
 
+print("Warning! If your files are not aligned PERFECTLY then this tool will not work properly!")

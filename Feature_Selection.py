9981b99d0fb0007d9e08a569a39a6d4f1274a9b5
DanGeospatial <daniel.nelson.ca@gmail.com>
2024-08-05 14:43:15 -0400

    work on Compiler.py and Feature_Selection.py

---
+++ Feature_Selection.py
@@ -0,0 +1,17 @@
+"""
+Remove features to improve accuracy and efficiency by dropping features that are unimportant, add error, etc.
+"""
+
+# Print column headers
+
+# Plot averages and stats
+
+# Remove variables that are known to have errors
+
+# Drop irrelevant features, i.e. lake temperature
+
+# Print remaining columns
+
+# Input into feature elimination
+
+# Output dataset of optimal variables

ed2637f0172e49d02527db0dac5fac84e9ff3246
DanGeospatial <daniel.nelson.ca@gmail.com>
2024-08-08 15:33:43 -0400

    finish Feature_Selection.py, start Feature_Elimination.py

--- Feature_Elimination.py
+++ Feature_Elimination.py
@@ -1,7 +1,8 @@
 """
 Remove features to improve accuracy and efficiency by dropping features that are unimportant, add error, etc.
 """
-
+from ray.train import ScalingConfig
+from ray.train.xgboost import XGBoostTrainer
 
 clean_path = "I:/Wildfire_Datasets_v2/combined_clean.csv"
 
